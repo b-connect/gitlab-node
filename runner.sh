95805ec50e3d371009e3abd4ac4b60bef69ff200
Erik Seifert <erik.seifert@b-connect.de>
2017-12-13 11:43:03 +0100

    better check for npm

--- runner.sh
+++ runner.sh
@@ -3,7 +3,13 @@
 for i in $(echo $NPM_BUILD_PATHS | tr "," "\n")
 do
   # process
-  cd $i;
-  npm i
-  npm run build:dev
+  if [ -e "${i}/package.json" ]
+  then
+    cd $i;
+    npm i
+    npm run build:dev
+  else
+    echo "No package"
+  fi
+
 done
